b133b2a43970f6cfa48b0c6ab95bb7e98dac068c
Simchanmin <simcm20003@gmail.com>
2025-03-24 14:00:52 +0900

    테스트

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int[] Array = {1,3,4,5,6,7};
+        for(int i=0; i<6; i++){
+            System.out.println(Array[i]);
+        }
+        
+
+
+
+
+    }
+}
