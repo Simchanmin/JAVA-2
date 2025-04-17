8b78d7856c73ef2cc7436cd60ef3133664e1dc24
Simchanmin <simcm20003@gmail.com>
2025-04-17 09:58:20 +0900

    Update asdf.java

--- src/asdf.java
+++ src/asdf.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
+
 public class asdf {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("This is a test.");
-        System.out.println("Goodbye, World!");
-        
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("이름 입력");
+        String name = scanner.nextLine(); // 이름을 먼저 입력받음
+
+        System.out.println("나이 입력");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // 남아 있는 개행 문자 제거
+
+        Person P1 = new Person(name, age);
+        P1.Print();
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person() {
+        this.name = "입력없음";
+        this.age = 0;
+    }
+
+    void Print() {
+        System.out.println("이름 :" + name);
+        System.out.println("나이 :" + age);
     }
 }
